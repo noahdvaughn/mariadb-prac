99c7c2e92a371cbaa58e2a2c4cf5a07576b0bc62
Noah Vaughn <noahdvaughn@gmail.com>
2024-06-22 19:12:45 -0400

    configure mariadb connector

--- server.ts
+++ server.ts
@@ -1,8 +1,29 @@
 import http from 'http'
+import mariadb from 'mariadb'
 
-const port = process.env.PORT || 3000 // Use environment variable or default port
+const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
+const pool = mariadb.createPool({
+  host: process.env.MARIA_HOST,
+  user: process.env.MARIA_USER,
+  password: process.env.MARIA_PW,
+  database: process.env.MARIA_DB
+})
+
+const server = http.createServer(async (req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello from TypeScript server!')
+  try {
+    const conn = await pool.getConnection()
+    const rows = await conn.query('SELECT 1 as val')
+    console.log('Connection successful:', rows)
+
+    conn.release()
+  } catch (err) {
+    console.error('Error connecting to MariaDB:', err)
+  }
+
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
   res.end('Hello from TypeScript server!')
